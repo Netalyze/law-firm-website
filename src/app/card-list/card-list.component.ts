11bb4a8709bf23aee2e164ec2329319792556342
rasiaq <48965961+rasiaq@users.noreply.github.com>
2020-09-12 21:12:50 +0200

    zmiana nazw obrazków

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -10,8 +10,8 @@ export class CardListComponent implements OnInit {
 
   cards: Card[] = [
     { image: '../../assets/images/card_1.png', title: 'Wypadki drogowe', description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
-    { image: '../../assets/images/card_2.jpg', title: 'Wypadki niedrogowe', description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
-    { image: '../../assets/images/card_3.jpg', title: 'Wypadki prawie drogowe', description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'}
+    { image: '../../assets/images/card_2.jpg', title: 'Sprawy rozwodowe', description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
+    { image: '../../assets/images/card_3.jpg', title: 'Prawo karne', description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'}
   ];
 
   constructor() { }
