ef920132107e970ff17ddb4e907d69d4de64f14c
Silkypaladin <bednarski.szymon99@gmail.com>
2020-10-09 18:06:34 +0200

    Dodany routing do kart, poprawiony wyglad
    Karty przekierowuja do poprawnych stron, design nie zmienia sie po kliknieciu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LeftCardComponent } from './left-card/left-card.component';
+import { MidCardComponent } from './mid-card/mid-card.component';
+import { RightCardComponent } from './right-card/right-card.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'left-card', component: LeftCardComponent},
+  { path: 'mid-card', component: MidCardComponent},
+  { path: 'right-card', component: RightCardComponent},
 ];
 
 @NgModule({
--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -9,9 +9,9 @@ import { Card } from '../card';
 export class CardListComponent implements OnInit {
 
   cards: Card[] = [
-    { image: '../../assets/images/card_1.png', title: 'Wypadki drogowe', link:"#",description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
-    { image: '../../assets/images/card_2.jpg', title: 'Sprawy rozwodowe',link: "#", description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
-    { image: '../../assets/images/card_3.jpg', title: 'Prawo karne', link: "#",description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'}
+    { image: '../../assets/images/card_1.png', title: 'Wypadki drogowe', link:"left-card",description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
+    { image: '../../assets/images/card_2.jpg', title: 'Sprawy rozwodowe',link: "mid-card", description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'},
+    { image: '../../assets/images/card_3.jpg', title: 'Prawo karne', link: "right-card",description:'Lorem ipsum dolor sit amet, ectetur adipiscing elit, sed do eiusmod tem incididunt ut labore et dolore.'}
   ];
 
   constructor() { }
