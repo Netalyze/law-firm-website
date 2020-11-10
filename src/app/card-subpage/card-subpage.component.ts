9496796739ed8eb6f73395213faaf99cd4e07cc4
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-11-10 11:58:42 +0100

    Wygenerowano card-subpage component

---
+++ src/app/card-subpage/card-subpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-subpage',
+  templateUrl: './card-subpage.component.html',
+  styleUrls: ['./card-subpage.component.css']
+})
+export class CardSubpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
