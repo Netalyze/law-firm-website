d83aa674dd99bc2c8632098e43f3a53de71645ef
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-11-26 20:53:32 +0100

    Dodanie sekcji nagród na stronę główną

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -9,7 +9,10 @@ import { Award } from '../award'
 export class AwardsComponent implements OnInit {
 
   awards: Award[] = [
-    
+    {icon: 'school', description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem veritatis voluptate eaque, id repellendus nulla sed dolores perspiciatis pariatur quisquam numquam facere, cupiditate voluptatibus? Sint est excepturi amet consectetur non!'},
+    {icon: 'brightness_auto', description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem veritatis voluptate eaque, id repellendus nulla sed dolores perspiciatis pariatur quisquam numquam facere, cupiditate voluptatibus? Sint est excepturi amet consectetur non!'},
+    {icon: 'verified_user', description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem veritatis voluptate eaque, id repellendus nulla sed dolores perspiciatis pariatur quisquam numquam facere, cupiditate voluptatibus? Sint est excepturi amet consectetur non!'},
+    {icon: 'emoji_events', description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem veritatis voluptate eaque, id repellendus nulla sed dolores perspiciatis pariatur quisquam numquam facere, cupiditate voluptatibus? Sint est excepturi amet consectetur non!'},
   ];
 
   constructor() { }
